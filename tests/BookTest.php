159697f62ab05f4810121e3e68e24afece2f85bf
Jonathan Lin & erica cha <student@epicodus.com>
2015-03-25 09:47:20 -0700

    Add test_setAuthorName();

--- tests/BookTest.php
+++ tests/BookTest.php
@@ -27,6 +27,24 @@
             $this->assertEquals("Jane Olstin", $result);
 
         }
+
+
+        function test_setAuthorName()
+        {
+            //Arrange
+            $author = "Erica Cha";
+            $title = "Charlotte's Web";
+            $id = null;
+            $test_book = new Book($author, $title, $id);
+            $new_author = "Jonathan Lin";
+
+            //act
+            $test_book->setAuthor($new_author);
+            $result = $test_book->getAuthor();
+
+            //assert
+            $this->assertEquals("Jonathan Lin", $result);
+        }
     }
 
 
